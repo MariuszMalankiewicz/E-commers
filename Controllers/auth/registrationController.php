4bb7226499f89afe12a6d2a6098b1a20c6be3e6d
MariuszMalankiewicz <m2ario123@wp.pl>
2023-07-23 17:06:47 +0200

    update RegisterController

--- Controllers/auth/registrationController.php
+++ Controllers/auth/registrationController.php
@@ -12,36 +12,57 @@ class RegistrationController
 
     public function addUser()
     {
-
-        $_SESSION['message'] = '';
-
-        if(isset($_POST['registration']))
+        if(isset($_POST['registration']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['repassword']))
         {
             $formData = 
             [
-
                 'username' => $_POST['username'],
                 'email' => $_POST['email'],
                 'password' => $_POST['password'],
                 'repassword' => $_POST['repassword']
-                
             ];
 
-            empty(Validation::trimData($formData)) ? $_SESSION['message'] = "Uzupełnij wszystkie pola" : '';
+            Validation::trimData($formData);
 
-            Validation::strlenString($formData['username'], 3, 15) ? $_SESSION['message'] = "Nazwa musi posiadać od 3 do 15 znaków" : '';
+            if(Validation::strlenString($formData['username'], 3, 15))
+            {
+                $_SESSION['message'] = "Nazwa musi posiadać od 3 do 15 znaków";
+            }
 
-            Validation::strlenString($formData['email'], 3, 30) ? $_SESSION['message'] = "Email musi posiadać od 3 do 30 znaków" : '';
+            else if(Validation::strlenString($formData['email'], 3, 30))
+            {
+                $_SESSION['message'] = "Email musi posiadać od 3 do 30 znaków";
+            }
 
-            !Validation::validEmail($formData['email']) ? $_SESSION['message'] = "Nieprawidłowy format email" : '';
+            else if(!Validation::validEmail($formData['email']))
+            {
+                $_SESSION['message'] = "Nieprawidłowy format email";
+            }
+            
+            else if(Validation::sameDataInDb($formData['email']))
+            {
+                $_SESSION['message'] = "Taki email już istnieje";
+            }
 
-            Validation::sameDataInDb($formData['email']) ? $_SESSION['message'] = "Taki email już istnieje" : '';
+            else if(Validation::strlenString($formData['password'], 3, 30))
+            {
+                $_SESSION['message'] = "Hasło musi posiadać od 3 do 30 znaków";
+            }
 
-            Validation::strlenString($formData['password'], 3, 30) ? $_SESSION['message'] = "Hasło musi posiadać od 3 do 30 znaków" : '';
+            else if(Validation::strlenString($formData['repassword'], 3, 30))
+            {
+                $_SESSION['message'] = "Powtórz Hasło musi posiadać od 3 do 30 znaków";
+            }
 
-            Validation::strlenString($formData['repassword'], 3, 30) ? $_SESSION['message'] = "Powtórz Hasło musi posiadać od 3 do 30 znaków" : '';
+            else if(Validation::differentData($formData['password'], $formData['repassword']))
+            {
+                $_SESSION['message'] = "Hasła nie są takie same";
+            }
 
-            Validation::differentData($formData['password'], $formData['repassword']) ? $_SESSION['message'] = "Hasła nie są takie same" : '';
+            else
+            {
+                $_SESSION['message'] = "";
+            }
 
             if($_SESSION['message'] === '')
             {
@@ -68,7 +89,11 @@ class RegistrationController
                     exit();
             }
 
-
+        }
+        
+        else
+        {
+            $_SESSION['message'] = 'Uzupełnij wszystkie pola';
         }
     
     }
